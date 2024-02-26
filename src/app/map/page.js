16a41b87062f7e4c8e9c14b7fd8d68ae20ff1033
Cheshnark <alejandrorubiosuela@gmail.com>
2024-02-26 12:32:03 +0100

    Update map page styles.

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -6,7 +6,7 @@ import LocationList from './components/LocationList/LocationList'
 const Map = () => {
   return (
     <main className={styles.map}>
-      <h2 className='font-berkshire mb-6'>Map</h2>
+      <h2 className='font-berkshire mb-6 text-center'>Map</h2>
       <div className={`${styles.mapContainer} md:flex gap-4`}>
         <LocationList />
         <MapLeaf />
