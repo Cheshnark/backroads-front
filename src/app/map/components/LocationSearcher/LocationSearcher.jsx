84c4d507700c807059d4198e359b5486dcc06404
Cheshnark <alejandrorubiosuela@gmail.com>
2024-03-01 20:53:22 +0100

    Update LocationSearcher, searches and gets coordinates.

--- src/app/map/components/LocationSearcher/LocationSearcher.jsx
+++ src/app/map/components/LocationSearcher/LocationSearcher.jsx
@@ -2,7 +2,7 @@
 
 import styles from './LocationSearcher.module.css'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 
 import { getCoordinates } from '@/utils/geocode'
@@ -11,37 +11,27 @@ const LocationSearcher = () => {
   const [locations, setLocations] = useState([])
   const [query, setQuery] = useState('')
 
-  useEffect(() => {
-    // const keys = ['title', 'body', 'locationType']
-    // const filtered = locations.filter((location) =>
-    //   keys.some(key => location[key].toLowerCase().includes(query)))
-
-    const fetchLocations = async () => {
-      const res = await axios.get(`http://127.0.0.1:8000/api/location/locations?q=${query}`)
-      let data = []
-      if (!query) {
-        data = await res.data.data
-      } else {
-        data = await res.data
-      }
-
-      setLocations(data)
+  const handleClick = async () => {
+    const res = await axios.get(`http://127.0.0.1:8000/api/location/locations?q=${query}`)
+    let data = []
+    if (!query) {
+      data = await res.data.data
+    } else {
+      data = await res.data
     }
 
-    // setTimeout(async () => {
-    //   const finalLocation = locations[0]?.address
-    //   const coordinates = await getCoordinates(finalLocation)
+    setLocations(data)
 
-    //   console.log(coordinates)
-    // }, 2000)
+    const finalLocation = locations[0]?.address
+    const coordinates = await getCoordinates(finalLocation)
 
-    fetchLocations()
-  }, [query])
+    console.log(coordinates[0].lat, coordinates[1].lat)
+  }
 
   return (
     <div className={styles.locationSearcher}>
       <input type='text' placeholder='Search shelters' onChange={(e) => setQuery(e.target.value.toLocaleLowerCase())} />
-      <button>Clicky</button>
+      <button onClick={handleClick}>Clicky</button>
 
       <ol>
         {locations?.map(location => (
