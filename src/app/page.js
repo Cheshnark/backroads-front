8fd331a7cc9ce660b50de87bc06771c7df544049
Cheshnark <alejandrorubiosuela@gmail.com>
2024-03-10 19:17:06 +0100

    Update main page front and honor code sections.

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,10 @@ const Home = () => {
     <main className={styles.main}>
       <section className={`${styles.frontContainer} flex flex--col justify-center items-end`}>
         <div className={`${styles.cards} flex flex-col sm:flex-row justify-center gap-4`}>
+          <div className={`${styles.boxText}`}>
+            <h2 className='font-berkshire'>Backroads</h2>
+            <p><span className='font-semibold'>Burn rubber, not bridges</span>. Backroads connects you with a network of badass shelters – think hidden cabins, biker-friendly barns, and epic campsites – perfect for when your adventure takes an unexpected turn. No suits, no rules, just hardcore hospitality for riders who bleed wanderlust.  Download Backroads and keep the wild in your ride.</p>
+          </div>
         </div>
       </section>
       <section className={`${styles.shelterContainer} flex flex-col justify-center items-center`}>
@@ -20,10 +24,10 @@ const Home = () => {
             <h2 className='font-berkshire'>Honor Code</h2>
             <p>Check our traveler honor code. We may be wild and stuff, but we are not chaos marauders, there&apos;s honor in our way.</p>
             <button><Link href={'/honor-code'}>Check it</Link></button>
+          </div>
         </div>
-      </div>
-    </section>
-    </main >
+      </section>
+    </main>
   )
 }
 
